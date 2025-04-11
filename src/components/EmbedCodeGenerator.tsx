d0e95345cb634eac083dc7e94b6b9a2be136bdb3
Your Name <user@example.com>
2025-04-11 01:18:39 +0100

    Hardcode working iframe format in EmbedCodeGenerator

--- src/components/EmbedCodeGenerator.tsx
+++ src/components/EmbedCodeGenerator.tsx
@@ -241,22 +241,8 @@ const EmbedCodeGenerator: React.FC<EmbedCodeGeneratorProps> = ({ title, mapId })
   }, [mapId, scaleTitle, minLabel, maxLabel, tooltipDescription]);
 
   const generateEmbedCode = () => {
-    // Direct URL to the embed page - without hash marks
-    const baseUrl = "https://map-generator-umber.vercel.app";
-    
-    // Build query parameters
-    const params = new URLSearchParams();
-    if (mapId) params.append('id', mapId);
-    if (scaleTitle) params.append('scaleTitle', scaleTitle);
-    if (minLabel) params.append('minLabel', minLabel);
-    if (maxLabel) params.append('maxLabel', maxLabel);
-    if (tooltipDescription) params.append('tooltipDescription', tooltipDescription);
-    
-    const queryString = params.toString() ? `?${params.toString()}` : '';
-    const embedUrl = `${baseUrl}/embed${queryString}`;
-    
-    // Simple iframe format that works well with Shopify
-    return `<iframe src="${embedUrl}" width="100%" style="min-height:500px" frameborder="0" allowfullscreen></iframe>`;
+    // Hard-coded, known-working iframe code format that works with Shopify
+    return `<iframe src="https://map-generator-umber.vercel.app/embed" width="100%" style="min-height:500px" frameborder="0" allowfullscreen></iframe>`;
   };
   
   const handleCopyCode = () => {
