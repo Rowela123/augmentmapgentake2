f68eed59a94bd60efa30a44b351e57143b6b3c1b
Rowela123 <alanrowellmain@gmail.com>
2025-05-12 22:52:18 +0100

    Add sideHustleData with correct StateData format

---
+++ src/data/sideHustleData.ts
@@ -0,0 +1,87 @@
+import { StateData } from '../types';
+
+/**
+ * Sample dataset showing most popular side hustles by state
+ */
+export const sideHustleData: StateData[] = [
+  {
+    state: 'AL',
+    value: 680,
+    fullName: 'Alabama',
+    label: 'DoorDash Delivery',
+    info: 'Average monthly income: $680',
+    color: '#FF3D00'
+  },
+  {
+    state: 'AK',
+    value: 950,
+    fullName: 'Alaska',
+    label: 'Freelance Photography',
+    info: 'Average monthly income: $950',
+    color: '#FFA000'
+  },
+  {
+    state: 'AZ',
+    value: 1200,
+    fullName: 'Arizona',
+    label: 'Uber/Lyft Driver',
+    info: 'Average monthly income: $1,200',
+    color: '#FFD600'
+  },
+  {
+    state: 'AR',
+    value: 620,
+    fullName: 'Arkansas',
+    label: 'Online Surveys',
+    info: 'Average monthly income: $620',
+    color: '#FF3D00'
+  },
+  {
+    state: 'CA',
+    value: 1800,
+    fullName: 'California',
+    label: 'Web Development',
+    info: 'Average monthly income: $1,800',
+    color: '#00C853'
+  },
+  {
+    state: 'CO',
+    value: 1400,
+    fullName: 'Colorado',
+    label: 'Dog Walking',
+    info: 'Average monthly income: $1,400',
+    color: '#00E676'
+  },
+  {
+    state: 'CT',
+    value: 1350,
+    fullName: 'Connecticut',
+    label: 'Tutoring',
+    info: 'Average monthly income: $1,350',
+    color: '#00E676'
+  },
+  {
+    state: 'DE',
+    value: 980,
+    fullName: 'Delaware',
+    label: 'Food Delivery',
+    info: 'Average monthly income: $980',
+    color: '#FFA000'
+  },
+  {
+    state: 'FL',
+    value: 1250,
+    fullName: 'Florida',
+    label: 'Airbnb Hosting',
+    info: 'Average monthly income: $1,250',
+    color: '#FFD600'
+  },
+  {
+    state: 'GA',
+    value: 1100,
+    fullName: 'Georgia',
+    label: 'Freelance Writing',
+    info: 'Average monthly income: $1,100',
+    color: '#FFD600'
+  }
+];
